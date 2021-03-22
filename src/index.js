688ab323f9336766c9fe4e232e521e426dc8f6e7
Erico Almeida <erico.silva@invillia.com>
2021-03-22 08:10:51 -0400

    refacored repositories project

--- src/index.js
+++ src/index.js
@@ -32,11 +32,15 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryExists = repositories.some((repository) => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repositoryExists) {
     return response.status(404).json({ error: "Repository not found" });
   }
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (!!updatedRepository.likes) delete updatedRepository.likes
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
@@ -48,11 +52,13 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+ const repositoryExists = repositories.some((repository) => repository.id === id);
 
-  if (repositoryIndex > 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
+ if(!repositoryExists){
+   return response.status(404).json({error: 'Repository not found!'});
+ }
+ 
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   repositories.splice(repositoryIndex, 1);
 
@@ -70,7 +76,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
